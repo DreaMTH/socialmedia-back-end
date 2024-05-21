e4913bf38cf9a6a55b8e93f0a1f8006dc90bcf31
dreamth <d.dimcard@gmail.com>
2024-05-21 15:19:17 +0300

    routers with endpoints

---
+++ src/Routes/postRouter.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from "express";
+import { postModel } from "../Models/postModel";
+import { checkAuth } from "../middleware/auth";
+
+const postRouter = express.Router();
+
+postRouter.post('/create', checkAuth, async (req: Request, res: Response) => {
+    try {
+        const { post } = req.body;
+        const doc = new postModel({
+            createdBy: (<any>req).userId,
+            post,
+        });
+        const newPost = await doc.save();
+        if (!newPost) {
+            return res.status(400).json({
+                message: "Unable to save post",
+            })
+        }
+        return res.status(203).json({ ...newPost });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            message: "Unable to create post",
+        })
+    } finally {
+        console.log("Attempt to create post");
+    }
+});
+postRouter.get('/', checkAuth, async (req: Request, res: Response) => {
+    try {
+        const posts = await postModel.find();
+        if (!posts) {
+            return res.status(500).json({
+                message: "Unable to get posts",
+            })
+        }
+        return res.status(200).json(posts);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            message: "Unable to get posts",
+        })
+    } finally {
+        console.log("Attempt to get posts");
+    }
+})
---
+++ src/Routes/userRouter.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import { userModel } from "../Models/userModel";
+import { checkAuth } from "../middleware/auth";
+import { isValidObjectId } from "mongoose";
+
+const userRouter = express.Router();
+
+userRouter.get('/:id', checkAuth, async (req: Request, res: Response) => {
+    try {
+        if (!isValidObjectId(req.params.id)) {
+            return res
+                .status(404).json({
+                    message: "Bad request",
+                })
+        }
+        const user = await userModel.findById(req.params.id);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ message: "No user with such id" });
+        }
+        return res
+            .status(200)
+            .json({ ...user });
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(500).json({
+                message: "Unable to get user",
+            });
+    } finally {
+        console.log("Attempt to get user");
+    }
+});
